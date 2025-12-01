036e394222cd737dda6d66ed75693a99a30f066a
ESSAFI01 <essafianas68@gmail.com>
2025-12-01 21:31:55 +0100

    model predictions

--- backend/src/predict.py
+++ backend/src/predict.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import pickle
 
-def load_model(model_path='C:\Users\essaf\Desktop\MLopsTP\Mlpro\models\regressorfinal.pkl'):
+def load_model(model_path=r'Mlpro\models\regressorfinal.pkl'):
     """
     Charge le modèle, scaler et encoders
     """
@@ -67,7 +67,7 @@ def predict_multiple(test_configs, model_data):
             'Mileage': f"{config['kilometerage']:.0f}",
             'Engine': config['engine'],
             'Transmission': config['transmission'],
-            'Estimated Price': f"{price:.0f} MAD"
+            'Estimated Price': f"{price:.0f} EUR"
         })
     
     return pd.DataFrame(results)
@@ -77,13 +77,12 @@ if __name__ == "__main__":
     model_data = load_model()
     
     test_configs = [
-        {'name': 'Mercedes-Benz C Class', 'manufacturer': 'MERCEDES-BENZ', 'age': 5, 
-         'kilometerage': 50000.0, 'engine': 'Petrol', 'transmission': 'Automatic'},
-        {'name': 'Toyota Corolla', 'manufacturer': 'TOYOTA', 'age': 5, 
-         'kilometerage': 50000.0, 'engine': 'Petrol', 'transmission': 'Manual'},
         {'name': 'Ford Fiesta', 'manufacturer': 'FORD', 'age': 5, 
-         'kilometerage': 50000.0, 'engine': 'Petrol', 'transmission': 'Manual'}
+         'kilometerage': 5000.0, 'engine': 'Petrol', 'transmission': 'Automatic'},
+        {'name': 'Vauxhall Corsa', 'manufacturer': 'VAUXHALL', 'age': 3, 
+         'kilometerage': 30000.0, 'engine': 'Petrol', 'transmission': 'Manual'},
     ]
+    
     
     results = predict_multiple(test_configs, model_data)
     print("\n🚗 Prédictions :")
